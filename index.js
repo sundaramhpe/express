a1f8ba34508007fd76544fe87fc82030d81997d4
sundaramhpe <sunram_c@hotmail.com>
2020-03-12 23:21:47 +0530

    morgan third party middleware installed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app=express();
 
+//miidleware  
+app.use((req,res,next) =>{
+    console.log('Middleware passing with different rendered page')
+    next()
+})
+
 //spcifiy the path
 app.get('/',(req, res)=>{
 res.json('This is first Express Page ')
