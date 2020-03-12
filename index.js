c19824a44b7b6762431de32a531abf57c6293ff7
sundaramhpe <sunram_c@hotmail.com>
2020-03-12 23:05:05 +0530

    specify the path

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app=express();
 
+//spcifiy the path
+app.get('/',(req, res)=>{
+res.json('This is first Express Page ')
+})
+
+app.get('/about',(req, res)=>{
+    res.json('This is About  Page rendered ')
+    })
+
 //add port
 app.listen('2000',()=>{
     console.log("Server started on port 2000 ")
